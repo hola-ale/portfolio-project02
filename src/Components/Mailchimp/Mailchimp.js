8c56aa1d6aa7b5e612802ccbbd5d375b4e53c63a
Aleksandra Hola <homoncik.aleksandra@egmail.com>
2022-05-04 12:21:37 +0200

    add embeded mailchimp

---
+++ src/Components/Mailchimp/Mailchimp.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import "./Mailchimp.css";
+
+const CustomForm = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      firstName &&
+      email.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email,
+        MERGE1: firstName,
+      });
+  };
+
+  useEffect(() => {
+    if (status === "success") clearFields();
+  }, [status]);
+
+  const clearFields = () => {
+    setFirstName("");
+    setEmail("");
+  };
+
+  return (
+    <form className="form" onSubmit={(e) => handleSubmit(e)}>
+      <h3>
+        {status === "success"
+          ? "Success!"
+          : "Join our email list for future updates."}
+      </h3>
+      {status === "sending" && (
+        <div className="alert alert--sending">sending...</div>
+      )}
+      {status === "error" && (
+        <div
+          className="alert alert--error"
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" && (
+        <div
+          className="alert alert--success"
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status !== "success" ? (
+        <div className="field-container">
+          <label>
+            First name:
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChangeHandler={setFirstName}
+              placeholder="Jane"
+              isRequired
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChangeHandler={setEmail}
+              placeholder="your@email.com"
+              isRequired
+            />
+          </label>
+        </div>
+      ) : null}
+      <input name="subscribe" type="submit" value="Submit" />
+    </form>
+  );
+};
+
+const MailchimpFormContainer = (props) => {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_ACTION_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+  return (
+    <div className="form-container">
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={(formData) => subscribe(formData)}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default MailchimpFormContainer;
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 import "./Post.css";
 
 export default function Post() {
-    const [postData, setPostData] = useState(null);
-    
-    useEffect(() => {
-        sanityClient 
-            .fetch(`*[_type == "post"]{
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
                 title,
                 slug,
                 mainImage{
@@ -18,32 +19,35 @@ export default function Post() {
                     },
                     alt
                 }
-            }`)
-            .then((data) => setPostData(data))
-            .catch(console.error);
-    }, [postData]);
-    return(
-        <main>
-            <section>
-                <h1 className="page-title">Blog Posts Page</h1>
-                <div className="posts-grid">
-                    {postData && postData.map((post, index) => ( 
-                    <article className="single-post">
-                        <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                            <span key={index}>
-                                <img
-                                className="post-img"
-                                src={post.mainImage.asset.url}
-                                alt={post.mainImage.alt}/>
-                                <span>
-                                    <h3>{post.title}</h3>
-                                </span>
-                            </span>
-                        </Link>
-                    </article>
-                    ))}       
-                </div>
-            </section>
-        </main>
-    );
+            }`
+      )
+      .then((data) => setPostData(data))
+      .catch(console.error);
+  }, [postData]);
+  return (
+    <main>
+      <section>
+        <h1 className="page-title">Blog Posts Page</h1>
+        <div className="posts-grid">
+          {postData &&
+            postData.map((post, index) => (
+              <article className="single-post">
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <img
+                      className="post-img"
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                    />
+                    <span>
+                      <h3>{post.title}</h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
 }
